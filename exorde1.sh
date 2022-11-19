8808d6edc3d505ca1e0cffd0de2bf647a983abdf
zycode08 <zycode08@gmail.com>
2022-11-19 15:09:37 -0600

    update

---
+++ exorde1.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+PS3='Select an action: '
+options=(
+"Install Node"
+"Restart Node"
+"Check Log"
+"Exit")
+
+select opt in "${options[@]}"
+do
+case $opt in
+
+"Install Node")
+
+echo -e "\e[1m\e[32m	Enter eth address:\e[0m"
+echo "_|-_|-_|-_|-_|-_|-_|"
+read addr
+echo "_|-_|-_|-_|-_|-_|-_|"
+
+echo -e "\e[1m\e[32m    Enter worker number:\e[0m"
+echo "_|-_|-_|-_|-_|-_|-_|"
+read num
+echo "_|-_|-_|-_|-_|-_|-_|"
+
+wget -O get-docker.sh https://get.docker.com
+sudo bash get-docker.sh
+
+echo 'export ETH_ADDR='${addr} >> $HOME/.profile
+source $HOME/.profile
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+for((i=1; i<=$num; i++)); do
+  echo "========== Start worker $i =========="
+  docker run -d --restart unless-stopped --pull always --name exorde-cli-$i rg.fr-par.scw.cloud/exorde-labs/exorde-cli -m $ETH_ADDR -l 3
+done
+
+crontab -l > restartcron
+echo "0 */12 * * * bash /home/${USER}/restart.sh" >> restartcron
+crontab restartcron
+rm restartcron
+
+
+break
+;;
+
+"Restart Node")
+
+echo -e "\e[1m\e[32m    Enter worker number:\e[0m"
+echo "_|-_|-_|-_|-_|-_|-_|"
+read num
+echo "_|-_|-_|-_|-_|-_|-_|"
+
+source ~/.profile
+
+for((i=1; i<=$num; i++)); do
+  echo "========== Restart worker $i =========="
+  docker_id=$(docker ps -a | grep "exorde-cli-${i}$" | cut -d ' ' -f 1)
+  echo "$docker_id"
+
+  if [[ -n $docker_id ]]
+  then
+    echo "delete old instance"
+    docker stop $docker_id
+    docker rm $docker_id
+  else
+    echo "docker instance is empty"
+  fi
+
+  docker run -d --restart unless-stopped --pull always --name exorde-cli-$i rg.fr-par.scw.cloud/exorde-labs/exorde-cli -m $ETH_ADDR -l 3
+done
+
+
+break
+;;
+
+"Check Log")
+
+echo -e "\e[1m\e[32m    Enter worker id:\e[0m"
+echo "_|-_|-_|-_|-_|-_|-_|"
+read wid
+echo "_|-_|-_|-_|-_|-_|-_|"
+
+docker_id=$(docker ps | grep "exorde-cli-${wid}$" | cut -d ' ' -f 1)
+echo "$docker_id"
+docker logs $docker_id -f
+
+break
+;;
+
+"Exit")
+exit
+
+esac
+done
--- restart.sh
+++ restart.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 source ~/.profile
-bash ./exorde.sh << EOF
+
+FILE="rlog.txt"
+
+echo "===== Restart Nodes on $(date) =====" >> $FILE
+bash ./exorde.sh << EOF &>> $FILE
 2
 2
 EOF
+echo -e "\n" >> $FILE
+
